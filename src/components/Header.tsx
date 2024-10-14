ac8c0d5ca5b6b4b05e6dcb8e55acc3f00b4d645c
jcserv <jarrod.servilla@gmail.com>
2024-10-14 13:06:19 -0400

    infra: add routing

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,25 +1,35 @@
 import { Github, GithubIcon } from "lucide-react";
 import { ModeToggle } from "./mode-toggle";
 import { Button } from "./ui/button";
+import { Link } from "@tanstack/react-router";
 
 export const Header: React.FC = () => {
   return (
-    <section className="flex items-center justify-between m-4">
+    <header className="flex items-center justify-between m-4">
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
         DevBlog
       </h1>
-      <div>
+      <div className="space-x-2">
+        <Link to="/">
+          Home
+        </Link>{" "}
+        <Link to="/about">
+          About
+        </Link>
         <Button
           variant="ghost"
           className="p-2"
           onClick={() => {
-            window.open("https://github.com/jcserv/react-vite-shadcn-template", "_blank");
+            window.open(
+              "https://github.com/jcserv/react-vite-shadcn-template",
+              "_blank"
+            );
           }}
         >
           <Github className="w-4 h-4" />
         </Button>
         <ModeToggle />
       </div>
-    </section>
+    </header>
   );
 };
